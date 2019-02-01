815a937fe55800504b83c6501baa4961d5876ddc
565407548@qq.com <565407548@qq.com>
2019-02-01 14:34:11 +0800

    add Hystrix, HystrixDashboard

--- zuul-service/src/main/java/com/spmia/zuul/ZuulApplication.java
+++ zuul-service/src/main/java/com/spmia/zuul/ZuulApplication.java
@@ -4,6 +4,8 @@ package com.spmia.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ZuulApplication {
 
     /**
